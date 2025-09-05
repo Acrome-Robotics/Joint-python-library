40053c4742a9ada67e90cb3c939acab6b00cdb04
Berat Computer <berat.dogan@std.yildiz.edu.tr>
2025-09-05 12:50:38 +0300

    joint device renamed

---
+++ test/joint_test.py
@@ -0,0 +1,28 @@
+from joint_devices.joint import *
+
+port = SerialPort("COM14", baudrate=921600, timeout=0.1, isTest=False)
+dev = Joint(0, port)
+
+
+
+
+
+while True:
+    #input()
+    print(dev.get_FOC_parameters(0))
+    #time.sleep(0.01)
+
+
+
+
+
+
+#blue.set_variables([Index_Blue.Config_TimeStamp, 123])
+#blue.set_variables([Index_Blue.Config_Description, ''])
+
+#blue.set_config_description()
+#blue.set_config_timeStamp()
+
+
+
+
