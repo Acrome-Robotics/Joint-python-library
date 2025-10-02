1cfbf32f05996839f650f451eec21a4580da7e76
Berat Computer <berat.dogan@std.yildiz.edu.tr>
2025-10-02 17:34:28 +0300

    tested

--- test/combined_scurve.py
+++ test/combined_scurve.py
@@ -4,17 +4,19 @@ from gui.ramp_trajectory import *   # <-- S-Curve Ramp sınıfını kullanacağ�
 keyword_for_usb = "USB-SERIAL"
 port = SerialPort(USB_serial_port(keyword_for_usb), baudrate=921600, timeout=0.01)
 
-
 joint_0 = Joint(0, port)
 joint_1 = Joint(1, port)
 
-
+print(joint_1.ping())
+print(joint_0.ping())
 # S curve setpoint
+joint_0.enable_torque(True)
+joint_1.enable_torque(True)
 
 while True:
-    print(joint_0.ping())
-    print(joint_1.ping())
-
+    sp = input("enter position setpoint : ")
+    joint_0.set_variables([Index_Joint.setpoint_position, float(sp)])
+    joint_1.set_variables([Index_Joint.setpoint_position, float(sp)])
 
 
 
